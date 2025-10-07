4d5ca12edb4a487b4d3d118e0586312357045e58
cirashrii <cirashrii@gmail.com>
2025-10-07 12:33:00 +0200

    Oppgave c gjennomført

--- src/main/java/no/hvl/dat100/javel/oppgave1/DailyPower.java
+++ src/main/java/no/hvl/dat100/javel/oppgave1/DailyPower.java
@@ -22,7 +22,9 @@ public class DailyPower {
 
         double sum = 0;
 
-        // TODO
+        for(int i = 0; i < usage.length; i++) {
+            sum += usage[i];
+        }
 
         return sum;
     }
