5af9ea1a16d4376474b6a7a62d7d2b31442a0c02
sol-br <solfridbryn98@gmail.com>
2025-10-17 12:13:10 +0200

    oppgave 1h + testdata oppg 1

--- src/main/java/no/hvl/dat100/javel/oppgave1/DailyPower.java
+++ src/main/java/no/hvl/dat100/javel/oppgave1/DailyPower.java
@@ -87,7 +87,11 @@ public class DailyPower {
 
         double temp_max = 0;
 
-        // TODO
+        for (int i = 0; i < usage.length; i++) {
+            if (usage[i] > temp_max) {
+                temp_max = usage[i];
+            }
+        }
 
         return temp_max;
     }
--- src/main/java/no/hvl/dat100/javel/oppgave1/DayMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave1/DayMain.java
@@ -28,8 +28,9 @@ public class DayMain {
         /*
         Solfrid:
         */
-
-
+        DailyPower.printPowerUsage(powerusage_day);
+        System.out.printf("Det totale strømforbruket: %.2f kwh%n", DailyPower.computePowerUsage(powerusage_day));
+        System.out.printf("Største strømforbruk i dag: %.2f kwh%n", DailyPower.computePowerUsage(powerusage_day));
 
 
         /*
